68a1c4c9e98e29031f1edebf200e98a972f80f69
EdoToji <92823614+EdoToji@users.noreply.github.com>
2022-06-19 22:50:56 -0700

    menu system
    created the basic user interface system

--- menu.java
+++
@@ -1,1 +0,0 @@
-//aiofbapiFBCOBF SA
---
+++ userInterface.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class userInterface{
+    static Scanner x = new Scanner(System.in);
+    static String input;
+    public static void display(){
+        boolean quit = false;
+        while(quit == false){
+            System.out.println("Enter 't' to display tenants" );
+            System.out.println("Enter 'r' to display rents" );
+            System.out.println("Enter 'e' to display expenses" );
+            System.out.println("Enter 'a' to annual report" );
+            input = x.nextLine();
+
+            switch(input){
+                case "t":
+                    //display tenant
+                    quit = true;
+                    break;
+                case "r":
+                    //display payment
+                    quit = true;
+                    break;
+                case "e":
+                    //display expense
+                    quit = true;
+                    break;
+                case "a":
+                    //display annual report
+                    quit = true;
+                    break;
+                default:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    public static void add(){
+        boolean quit = false;
+        while(quit == false){
+            System.out.println("Enter 't' to add tenant" );
+            System.out.println("Enter 'r' to record rent payment" );
+            System.out.println("Enter 'e' to record expense" );
+            input = x.nextLine();
+
+            switch(input){
+                case "t":
+                    //add tenant
+                    quit = true;
+                    break;
+                case "r":
+                    //rent payment
+                    quit = true;
+                    break;
+                case "e":
+                    //expense
+                    quit = true;
+                    break;
+                default:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    public static void menu(){
+        boolean quit = false;
+        
+        while(quit == false){
+            System.out.println("Enter 'i' to input data" );
+            System.out.println("Enter 'd' to display data" );
+            System.out.println("Enter 'q' to quit" );
+            input = x.nextLine();
+                
+            switch(input){
+                case "i":
+                    add();
+                    break;
+                case "d":
+                    display();
+                    break;
+                case "q":
+                    System.out.println("Have a good day!");
+                    quit = true;
+                    break;
+            }       
+        
+        }
+    }
+    public static void main(String[] args){
+        menu();
+    }
+}
