2a1954645221a29a6b47c3e2e5817f4f59a5ad95
Christine Bui <Christine.Bui@student.csulb.edu>
2022-06-25 13:41:27 -0700

    changes to expense.java and added display

--- expense.java
+++ expense.java
@@ -1,31 +1,60 @@
+import java.util.*;
+
 public class expense {
-    //hello
-    private double price;
-    private String date; //add the module date later and replace string
+    private double amount;
+    private Date date; 
     private String payee;
+    private String category;
 
     // constructor
     public expense(){
-        this.price = 0;
-        this.date = "no date";
+        this.amount = 0;
         this.payee = "";
+        this.category = "";
     };
 
     //loaded constructor
-    public expense(double price, String date, String payee){
-        this.price = price;
+    public expense(double amount, Date date, String payee){
+        this.amount = amount;
         this.date = date;
         this.payee = payee;
     }
-    //set and get fucnitons
+    //set and get functions
     /////////////////////////////////////////////////////////
-    public double getPrice(){return this.price;}
-    public void setPrice(double price){this.price = price;}
+    public double getAmount(){
+        return this.amount;
+    }
+    public void setAmount(double amount){
+        this.amount = amount;
+    }
     
-    public String getDate(){return this.date;}
-    public void setDate(String date){this.date = date;}
+    // public String getDate(){
+    //     return this.date;
+    // }
+    // public void setDate(String date){
+    //     this.date = date;
+    // }
 
-    public String getPayee(){return this.payee;}
-    public void setPayee(String payee){this.payee = payee;}
+    public String getPayee(){
+        return this.payee;
+    }
+    public void setPayee(String payee){
+        this.payee = payee;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
     ////////////////////////////////////////////////////////
+    // Display
+    public String toString()
+   {
+    System.out.println("Date\tPayee\tCategory");
+    System.out.println("----------------------------------------");
+   return super.toString() + this.date + "\t" + this.payee + "\t" + this.amount + "\t" + this.category;
+   }
 }
