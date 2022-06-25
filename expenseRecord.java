608eeab5f9373f53867a8df91102b289a9e74eaf
Thong Chau <80946011+HikoroZashi@users.noreply.github.com>
2022-06-24 22:56:57 -0700

    Create expenseRecord.java

---
+++ expenseRecord.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class expenseRecord {
+    List<expense> expenses = new ArrayList<expense>();
+    //tenant tenants[] = new tenant[5];
+    int i = 0;
+   public expenseRecord(){}
+    
+}
