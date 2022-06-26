1ca3fa29ca465f62bb5b218ba5ff861fd1af0bd1
Thong Chau <80946011+HikoroZashi@users.noreply.github.com>
2022-06-26 01:46:15 -0700

    Update expenseRecord.java

--- expenseRecord.java
+++ expenseRecord.java
@@ -12,6 +12,18 @@ public class expenseRecord {
 
     public void addExpense(double amount, GregorianCalendar date, String payee, String Category){
         expense e = new expense(amount, date, payee, Category);
+        expenses.add(e);
+        Collection.sort(e);
     }
     
+    
+   public void displayTenant(){
+    //System.out.println(tenants.toString());
+    System.out.println("date\tamount\tpayee\tcategory\n");
+    for(expense T: expenses){
+        System.out.println(T.toString());
+        }
+    }
+
+
 }
