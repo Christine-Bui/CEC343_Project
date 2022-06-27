754b040c9bb84bb61a74142b3b2cc41b51bc4a7b
Thong Chau <80946011+HikoroZashi@users.noreply.github.com>
2022-06-26 20:12:01 -0700

    finished annual report

--- annualReport.java
+++ annualReport.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class annualReport{
     private rentRecord rR;
     private expenseRecord eR;
+    HashMap<String,Double> e_C = new HashMap<String,Double>();
 
 
     //empty constructor
@@ -13,6 +14,8 @@ public class annualReport{
     public annualReport(rentRecord renRec, expenseRecord expRec){
         this.rR = renRec;
         this.eR = expRec;
+        
+        
     }
 
     //ook
@@ -28,18 +31,16 @@ public class annualReport{
     //hashmap<type1,type2> name = new Hashmap<>();
     //e_C = expense map divided by category to add up cost by each category
     //expense category
-    //returns a Hashmap, used to display by category
-    public HashMap expenseCategory(){
-        
-        HashMap<String,Double> e_C = new HashMap<String,Double>();
-        
+    //no return, affects class attributes
+    public void expenseCategory(){
+        //HashMap<String,Double> e_C = new HashMap<String,Double>();
         for(expense E : eR.expenses){
             String category = E.getCategory();
-            Double price = e_C.containsKey(category) ? e_C.get(category) : 0;
+            Double price = this.e_C.containsKey(category) ? this.e_C.get(category) : 0;
             price+=E.getAmount();
-            e_C.put(category, price);
+            this.e_C.put(category, price);
         }
-        return e_C;
+        //return e_C;
     }
 
     //used to calculate profit
@@ -58,5 +59,21 @@ public class annualReport{
         return profit;
     }
 
+    public void displayAnnualReport(){
+        System.out.println("Annual Summary\n-------------------------");
+        //loop to print out expense categories and hashmap
+        //printing Rent
+        System.out.println("Income");
+        System.out.println("Rent: " + this.sumRent());
+
+        //print expense category summary
+        System.out.println("Expenses");
+        for (String k : e_C.keySet()){
+            System.out.printf("%s: %.2f%n", k, e_C.get(k));
+        }
+
+        //print profit
+        System.out.println("\nBalance: " + this.profit());
+    }
 
 }
