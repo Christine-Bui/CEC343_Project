5af4928aae8856076c1a1d3f3e59eff39d2ea080
Thong Chau <80946011+HikoroZashi@users.noreply.github.com>
2022-06-26 18:04:16 -0700

    Update annualReport.java

--- annualReport.java
+++ annualReport.java
@@ -1,5 +1,3 @@
-// 
-
 public class annualReport{
     private rentRecord rR;
     private expenseRecord eR;
@@ -9,16 +7,28 @@ public class annualReport{
         this.rR = renRec;
         this.eR = expRec;
     }
-
+    
+    //ook
     public double sumRent(){
         double sum=0;
-        for(Rent R : rR){}
+        for(Rent R : rR.rental){
+            //sum+=R.getAmount();
+        }
         return sum;
     }
 
-    public double sumExp(){
+    
+    public double sumExpenses(){
         double sum=0;
-        for(expense E : eR){}
+        for(expense E : eR.expenses){
+            sum+=E.getAmount();
+        }
         return sum;
+    }
+    
+    //annualReport.profit();
+    public double profit(){
+        double profit = this.sumRent()-this.sumExpenses();
+        return profit;
     }
 }
