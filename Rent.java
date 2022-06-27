930ea5949b2c31023794f9a98a7a986401e56d59
EdoToji <92823614+EdoToji@users.noreply.github.com>
2022-06-26 21:22:56 -0700

    Added more to rent

--- Rent.java
+++ Rent.java
@@ -1,9 +1,36 @@
-public class Rent
-{
+public class Rent{
     private double amount;
-
-    public void Rent(){};
+    private String name;
+    private int month;
+    public Rent(){
+        
+    }
+    public Rent(String name, double amount, int month){
+        this.name = name;
+        this.amount = amount;
+        this.month = month;
+    }
     public double getAmount(){
         return amount;
-    };
+    }
+
+    public void setAmount(double amount){
+        this.amount = amount;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+
+    public void setMonth(int month){
+        this.month = month;
+    }
 }
--- rentRecord.java
+++ rentRecord.java
@@ -1,12 +1,13 @@
 import java.util.*;
 
-public class rentRecord
-{
+public class rentRecord extends tenantList{
     ArrayList<Rent> rental = new ArrayList<Rent>();
+    tenant t = new tenant();
+    tenantList tL = new tenantList();
 
-    public void addRent(int month, String name, double rent_amount)
+    public void addRent(String name, double rent_amount, int month)
     {
-        Rent r = new Rent(month, name, rent_amount);
+        Rent r = new Rent(name, rent_amount, month);
 
         if(rental.contains(r))
         {
