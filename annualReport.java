041e775274b9728f23f7a8c1529d0de9e20f81f8
Thong Chau <80946011+HikoroZashi@users.noreply.github.com>
2022-06-26 17:24:55 -0700

    Update annualReport.java

--- annualReport.java
+++ annualReport.java
@@ -1,4 +1,24 @@
-public class annualReport extends expenseRecord
-{
-    private rentRecord[] rent_record;
+// 
+
+public class annualReport{
+    private rentRecord rR;
+    private expenseRecord eR;
+
+    // constructor 
+    public void annualReport(rentRecord renRec, expenseRecord expRec){
+        this.rR = renRec;
+        this.eR = expRec;
+    }
+
+    public double sumRent(){
+        double sum=0;
+        for(Rent R : rR){}
+        return sum;
+    }
+
+    public double sumExp(){
+        double sum=0;
+        for(expense E : eR){}
+        return sum;
+    }
 }
